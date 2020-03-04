2390f7901e75f5d3eb94be4ffe5c3caede255b97
Brent Mazurek <brentmazurek@northwesternmutual.com>
2020-03-04 13:29:05 -0600

    SCSS partials and better BEM styles

--- src/pages/BEM.js
+++ src/pages/BEM.js
@@ -3,21 +3,33 @@ import React from 'react'
 function BEM() {
     return (
         <div className='content bem'>
-            <h1>Button styled with BEM CSS</h1>
-            <hr />
-            <Button text='Click me!'/>
-            <Button text='Warning button' color='orange' />
-            <Button text='Alert button! Something is wrong!' color='red' />
+            <h1 className='title'>Card and buttons styled with BEM CSS</h1>
+            <p>Using BEM makes it easier to block out your CSS. This prevents styling conflicts and makes for cleaner code that's easier to maintain.</p>
+            <Button className='top-button' color='green' text='Click here' />
+            <Card />
         </div>
     )
 }
 
 function Button(props) {
     return (
-        <button type='button' className={`btn ${props.color ? props.color : ''}`}>
+        <button type='button' className={`btn btn--${props.color ? props.color : ''} ${props.className}`}>
             <span>{props.text}</span>
         </button>
     )
 }
+
+const Card = () => {
+    return (
+      <div className='card'>
+        <img src='https://via.placeholder.com/368x280' alt='placeholder image' />
+        <div className='card__internal-content'>
+          <h5 className="card__title">Card title</h5>
+          <p className="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <Button className='card__btn card__btn--left-align' color='orange' text='Go somewhere' />
+        </div>
+      </div>
+    )
+  }
 
 export default BEM
